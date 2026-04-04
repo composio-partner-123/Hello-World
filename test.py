23191f39348d379114b94395d8e3141912da80b7
Test User Update 2 <partnerships@composio.dev>
2026-04-04 12:45:12 +0530

    Mixed operations with directory paths

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-def test_function():
-    """Test function with docstring."""
-    return True
-
-if __name__ == '__main__':
-    print('Tests passed!')
